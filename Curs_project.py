f7884acbfeb09ffe4172d7f536e2fca9c0a0f810
Живоглазов Игорь <igzhi@mail.ru>
2022-06-07 21:07:34 +0300

    Доработанный курсовой проект

--- Curs_project.py
+++ Curs_project.py
@@ -1,16 +1,19 @@
 import requests
 import time
-import json
 from datetime import date
-from pprint import pprint
 from tqdm import tqdm
 
+with open ('C:\\Users\\Igor & Alexa\\Desktop\\ИГОРЬ\\Нетология\\token_vk.txt', 'r') as f:
+    token_vk = f.read()
+
+
 def vk(vk_id, count):
-    """Функция получает информацию из аккаунта VK"""
+    """Функция получает информацию из аккаунта VK по ID или screen_name"""
+
     URL = 'https://api.vk.com/method/photos.get'
     params = {
         'owner_id': f'{vk_id}',
-        'access_token': 'a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd',
+        'access_token': f'{token_vk}',
         'v': '5.131',
         'album_id': 'profile',
         'extended': '1',
@@ -20,6 +23,19 @@ def vk(vk_id, count):
     res = requests.get(URL, params=params)
     data = res.json()
     info = data['response']['items']
+    return info
+
+def vk_id (vk_name):
+    """Функция получает ID аккаунта VK по screen_name"""
+    URL = 'https://api.vk.com/method/utils.resolveScreenName'
+    params = {
+        'screen_name': f'{vk_name}',
+        'access_token': f'{token_vk}',
+        'v': '5.131',
+    }
+    res = requests.get(URL, params=params)
+    data = res.json()
+    info = data['response']['object_id']
     return info
 
 def create_folder(token):
@@ -37,29 +53,48 @@ def upload_file(name1, url2, token):
     requests.post(upload_link, params=params, headers=header)
 
 def start():
-    vk_id = input('Введите id пользователя в ВКонтакте: ')
+    vk_i = input('Чтобы найти пользователя VK введите "i" для поиска по ID или "n" для поиска по screen_name: ')
+
+    if vk_i == 'i':
+        vkid = input('Введите ID пользователя: ')
+    else:
+        vk_name = input('Введите screen_name пользователя: ')
+        vkid = vk_id(vk_name)
+
     vk_count = input('Введите колличество необходимых фотографий: ')
     if not vk_count:
         vk_count = '5'
-    vk_info = vk(vk_id, vk_count)
+
+    vk_info = vk(vkid, vk_count)
     yandex_token = input('Введите token вашего Я.Диска: ')
     create_folder(yandex_token)
     names = []
+    type_size = []
     for i in vk_info:
         url_y = i['sizes'][-1]['url']
         na = str(i['likes']['count'])
+        type_s = i['sizes'][-1]['type']
+        type_size.append(type_s)
 
         if na not in names:
             name_y = i['likes']['count']
+            with open('json-файл.json', 'a') as file:
+                file.write(f'file_name: {name_y}.jpg ')
         else:
             name_y = date.fromtimestamp(i['date'])
+            with open('json-файл.json', 'a') as file:
+                file.write(f'file_name: {name_y}.jpg ')
         names.append(str(i['likes']['count']))
+
+        with open('json-файл.json', 'a') as file:
+            file.write(f'size: {type_s} \n')
 
         upload_file(name_y, url_y, yandex_token)
 
-    for q in tqdm(names):
-        time.sleep(1)
+    for _ in tqdm(vk_info):
+        time.sleep(0.5)
 
-start()
+if __name__ == "__main__":
+    start()
 
 
